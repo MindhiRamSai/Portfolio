0201c053d2a5758aa2ee98d5e2e7c543cbd0809b
Mindhi Ram Sai <ramsai03434@gmail.com>
2025-05-12 16:49:10 +0530

    Portfolio

---
+++ script.js
@@ -0,0 +1,50 @@
+let light=document.querySelector(".sun")
+let night=document.querySelector(".moon")
+let mode=document.querySelector(".Mode")
+let list=document.querySelector(".list")
+let remove=document.querySelector(".close")
+let menu=document.querySelector(".Menu")
+let links=document.querySelectorAll(".links")
+
+light.addEventListener('click',function(){
+    light.style.visibility= "hidden"
+    night.style.visibility= "visible"
+    mode.style.backgroundColor="black"
+    document.body.classList.add("Dark")
+    links.forEach(function(link){
+        link.classList.add("Dark")
+    })
+})
+
+night.addEventListener('click',function(){
+    night.style.visibility= "hidden"
+    light.style.visibility= "visible"
+    document.body.classList.remove("Dark")
+    mode.style.backgroundColor="transparent"
+    links.forEach(function(link){
+        link.classList.remove("Dark")
+    })
+})
+
+list.addEventListener('click',function(){
+    menu.style.display="flex"
+    remove.style.display="flex"
+    list.style.display="none"
+})
+
+remove.addEventListener('click',function(){
+    menu.style.display="none"
+    remove.style.display="none"
+    list.style.display="flex"
+})
+
+links.forEach(function(link){
+        link.addEventListener('click',function(){
+            if (window.innerWidth < 426){
+                menu.style.display="none"
+    remove.style.display="none"
+    list.style.display="flex"
+            }
+})
+    })
+
